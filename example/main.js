a0950880f75e90b9575cf00b03d08955283cd652
Samy Pessé <samypesse@gmail.com>
2016-04-01 16:49:13 +0200

    Add method to modify syntax of code blocks

--- example/main.js
+++ example/main.js
@@ -19,6 +19,18 @@ var Example = React.createClass({
         });
     },
 
+    // Edit syntax of a code block
+    onEditCodeSyntax: function(block) {
+        var syntax = MarkupEditor.CodeUtils.getSyntax(block);
+
+        // Get new syntax
+        syntax = window.prompt('Enter syntax for this code block', syntax);
+
+        this.onEditorChanged(
+            MarkupEditor.CodeUtils.editSyntax(this.state.editorState, block, syntax)
+        );
+    },
+
     render: function() {
         var editorState = this.state.editorState;
         var markdownString = editorState.getAsMarkdown();
@@ -27,7 +39,11 @@ var Example = React.createClass({
         return <div className="Example">
             <div className="Example-Editor">
                 <MarkupEditor.Toolbar editorState={editorState} onChange={this.onEditorChanged} />
-                <MarkupEditor editorState={editorState} onChange={this.onEditorChanged} />
+                <MarkupEditor
+                    editorState={editorState}
+                    onChange={this.onEditorChanged}
+                    onEditCodeSyntax={this.onEditCodeSyntax}
+                />
             </div>
             <div className="Example-Preview"><pre>{markdownString}</pre></div>
             <div className="Example-RawState"><pre>{JSON.stringify(rawContent, null, 4)}</pre></div>
